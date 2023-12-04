c9deb1ee4e50dcc7d3847e65de72aa090055d8f7
KDyulgerov <kdyulgerov.sport@gmail.com>
2023-12-04 10:42:18 +0200

    Upadated
    Updated

---
+++ 11.ExceptionsAndErrorHandling/01.SquareRoot/Program.cs
@@ -0,0 +1,24 @@
+﻿double SquareRoot(int number)
+{
+    return Math.Sqrt(number);
+}
+
+int input = int.Parse(Console.ReadLine());
+
+try
+{
+    if (input < 0)
+    {
+        throw new ArgumentException();
+    }
+    
+    Console.WriteLine(SquareRoot(input));
+}
+catch(ArgumentException)
+{
+    Console.WriteLine("Invalid number.");
+}
+finally
+{
+    Console.WriteLine("Goodbye.");
+}
---
+++ 11.ExceptionsAndErrorHandling/02.EnterNumbers/Program.cs
@@ -0,0 +1,39 @@
+﻿using System.Globalization;
+
+static int ReadNumber(int start, int end)
+{
+    int input = int.Parse(Console.ReadLine());
+
+    if (input <= start || input >= end)
+    {
+        throw new ArgumentException();
+    }
+
+    return input;
+}
+
+var numbers = new List<int>();
+
+int maxNum = 1;
+
+while (numbers.Count < 10)
+{
+    try
+    {
+        int num = ReadNumber(maxNum, 100);
+
+        numbers.Add(num);
+        maxNum = num;
+    }
+    catch (ArgumentException)
+    {
+        Console.WriteLine($"Your number is not in range {maxNum} - 100!");
+    }
+    catch (FormatException)
+    {
+        Console.WriteLine("Invalid Number!");
+    }
+
+}
+
+Console.WriteLine(string.Join(", ", numbers));
